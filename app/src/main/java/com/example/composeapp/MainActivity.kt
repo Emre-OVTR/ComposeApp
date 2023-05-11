1ff26d7b3966045631f1fcdd484362ac098ec293
Emre Turhal <you@example.com>
2023-05-11 17:12:34 +0200

    Initial commit

---
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -0,0 +1,106 @@
+package com.example.composeapp
+
+import android.content.res.Configuration
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeapp.ui.theme.ComposeAppTheme
+import com.example.composeapp.ui.theme.SampleData
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeAppTheme {
+                Surface (modifier = Modifier.fillMaxSize()){
+                    MessageCard(Message("Android", "Jetpack Compose"))
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun MessageCard(msg:com.example.composeapp.Message){
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape))
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author,
+            color = MaterialTheme.colors.secondaryVariant,
+            style = MaterialTheme.typography.subtitle2)
+
+            Spacer(modifier = Modifier.height(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    style = MaterialTheme.typography.body2)
+            }
+
+        }
+    }
+
+
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun previewMessageCard(){
+    ComposeAppTheme {
+        Surface {
+            MessageCard(
+                msg = Message("Colleague", "Hey, how are you?")
+            )
+        }
+    }
+
+}
+
+@Composable
+fun Conversation(messages:List<com.example.composeapp.Message>){
+    LazyColumn{
+        items(messages){ message ->
+            MessageCard(message)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewConversation(){
+    ComposeAppTheme {
+        Conversation(SampleData.conversationSample)
+    }
+}
+
