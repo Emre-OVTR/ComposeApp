ff98d353ebe3c7bc45e9e4a7073bc56b6e67bfb2
Emre Turhal <you@example.com>
2023-05-23 17:30:23 +0200

    create a list of messages

--- app/src/main/java/com/example/composeapp/MainActivity.kt
+++ app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.composeapp
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
@@ -17,80 +16,84 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
-import com.example.composeapp.ui.theme.SampleData
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeAppTheme {
-                Surface (modifier = Modifier.fillMaxSize()){
-                    MessageCard(Message("Android", "Jetpack Compose"))
+                Surface (modifier = Modifier.fillMaxSize()) {
+                    MessageCard(msg = Message(author = "Android", body = "Jetpack Compose"))
                 }
             }
+
         }
     }
 }
 
-data class Message(val author: String, val body: String)
+data class Message(val author : String, val body : String)
 
 @Composable
-fun MessageCard(msg:com.example.composeapp.Message){
+fun MessageCard(msg:Message){
     Row(modifier = Modifier.padding(all = 8.dp)) {
-        Image(painter = painterResource(id = R.drawable.profile_picture),
-            contentDescription = "Profile picture",
+        Image(
+            painter = painterResource(id = R.drawable.profile_picture),
+            contentDescription = "Contact profile picture",
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
-                .border(1.5.dp, MaterialTheme.colors.secondary, CircleShape))
+                .border(1.5.dp, MaterialTheme.colors.primary, CircleShape)
+        )
+
+
 
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column {
-            Text(text = msg.author,
-            color = MaterialTheme.colors.secondaryVariant,
-            style = MaterialTheme.typography.subtitle2)
+        Column() {
+            Text(text = msg.author, color = MaterialTheme.colors.secondary, style = MaterialTheme.typography.subtitle1)
 
             Spacer(modifier = Modifier.height(4.dp))
 
             Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
-                Text(text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    style = MaterialTheme.typography.body2)
+                Text(text = msg.body, modifier = Modifier.padding(all = 4.dp), style = MaterialTheme.typography.body1)
             }
 
         }
     }
-
-
 }
 
-@Preview(name = "Light Mode")
+@Preview(name ="Light Mode")
 @Preview(
     uiMode = Configuration.UI_MODE_NIGHT_YES,
     showBackground = true,
     name = "Dark Mode"
 )
 @Composable
-fun previewMessageCard(){
-    ComposeAppTheme {
-        Surface {
-            MessageCard(
-                msg = Message("Colleague", "Hey, how are you?")
-            )
+fun PreviewMessageCard(){
+    ComposeAppTheme() {
+        Surface() {
+            Column() {
+                MessageCard(
+                    msg = Message(
+                        "Lexi",
+                        "Hey, take a look at Jetpack Compose, it's great!"
+                    )
+                )
+            }
         }
     }
 
 }
 
 @Composable
-fun Conversation(messages:List<com.example.composeapp.Message>){
-    LazyColumn{
-        items(messages){ message ->
+fun Conversation(messages : List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
             MessageCard(message)
         }
     }
@@ -98,9 +101,12 @@ fun Conversation(messages:List<com.example.composeapp.Message>){
 
 @Preview
 @Composable
-fun PreviewConversation(){
-    ComposeAppTheme {
+fun previewConversation(){
+    ComposeAppTheme() {
         Conversation(SampleData.conversationSample)
     }
 }
+
+
+
 
